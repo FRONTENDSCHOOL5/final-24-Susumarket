f30f2a57260cd4a78288216fc7ce42c47b59014d
nastar <an1413@hs.ac.kr>
2023-06-15 15:13:21 +0900

    Feat: NotFound페이지 스타일드 컴포넌트추가

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import {
+  NotFoundWrapper,
+  NotFoundTitle,
+  NotFoundImage,
+  NotFoundP,
+} from "./notFound.style";
+import Button from "../../components/commons/button/Button";
+import Svg404 from "../../img/symbol-logo-404.svg";
 
 export default function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+  return <div>NotFound</div>;
 }
