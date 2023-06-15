5ae201f966115df8e9eb9b162e1610987d25ebcf
nastar <an1413@hs.ac.kr>
2023-06-15 15:13:21 +0900

    Feat: NotFound페이지 스타일드 컴포넌트추가

---
+++ src/pages/notFound/notFound.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const NotFoundWrapper = styled.article`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+export const NotFoundTitle = styled.h2`
+  width: 260px;
+  text-align: center;
+`;
+
+export const NotFoundImage = styled.img`
+  width: 100%;
+  margin-bottom: 12px;
+`;
+
+export const NotFoundP = styled.p`
+  font-size: 14px;
+  line-height: 1;
+`;
+
+export const NotFoundBtn = styled.button``;
