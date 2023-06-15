ba65b4a2a51e21290d52771bd61434d91cc6d687
nastar <an1413@hs.ac.kr>
2023-06-15 15:31:05 +0900

    Fix: Navigate 사용 후 이전페이지 수정

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -7,17 +7,18 @@ import {
 } from "./notFound.style";
 import Button from "../../components/commons/button/Button";
 import Svg404 from "../../img/symbol-logo-404.svg";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
     <>
       <NotFoundWrapper>
         <NotFoundTitle>
           <NotFoundImage src={Svg404} alt="" />
-          <NotFoundP>페이지를 찾을 수 없습니다 :)</NotFoundP>
+          <NotFoundP>페이지를 찾을 수 없습니다 :&#41;</NotFoundP>
         </NotFoundTitle>
-        <Button active={true} className="medium" onClick={() => Navigate(-1)}>
+        <Button active={true} className="medium" onClick={() => navigate(-1)}>
           이전 페이지
         </Button>
       </NotFoundWrapper>
