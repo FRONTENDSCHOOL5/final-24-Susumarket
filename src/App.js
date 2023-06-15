40e8130e4cbb86b8668c60cbc6e756fe4642a549
jongtae.nam <dark9737@gmail.com>
2023-06-15 17:44:26 +0900

    Feat: Context API UserProvider, ModalProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { GlobalStyle } from "./GlobalStyle";
 import Router from "./routes/Router";
-
+import { UserContext } from "./context/UserContext.jsx"
+import { ModalContext } from "./context/ModalContext.jsx";
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
+  const [isOpenPostModal, setIsOpenPostModal] = useState(false);
+
   return (
     <>
       <GlobalStyle />
-      <Router />
+      <UserContext.Provider value={{
+        accessToken, setAccessToken
+      }}> 
+      <ModalContext.Provider value={{ 
+        isOpenConfirmModal,
+        setIsOpenConfirmModal,
+        isOpenPostModal,
+        setIsOpenPostModal }}>
+        <Router />
+      </ModalContext.Provider>
+      </UserContext.Provider>
     </>
   );
 }
