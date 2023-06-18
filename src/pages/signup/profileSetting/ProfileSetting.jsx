44faf7f003fa816fefe1f0920707d55bfb658972
Yunjung324 <rachel324cc@naver.com>
2023-06-18 22:01:42 +0900

    Feat: 프로필, 사용자 이름, 계정id, 소개 api 코드 추가

--- src/pages/signup/profileSetting/ProfileSetting.jsx
+++ src/pages/signup/profileSetting/ProfileSetting.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../../components/commons/button/Button";
 import UserInput from "../../../components/commons/dataInput/UserInput";
 import DataInput from "../../../components/commons/dataInput/DataInput";
+import defaultimg from "../../../img/ProfileImg.svg";
+import uploadfile from "../../../img/upload-file.svg";
+import { customAxios } from "../../../library/customAxios";
+import { useLocation } from "react-router-dom";
 
 const ProfileContainer = styled.main`
   width: 87%;
@@ -36,25 +39,83 @@ const ProfileButton = styled(Button)`
   margin-top: 50px;
 `;
 
+const ImgLabel = styled.label`
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 50px;
+`;
+const ImgInput = styled.input`
+  display: none;
+`;
+
+const Img = styled.img`
+  &.defaultlion {
+    position: absolute;
+    margin-left: -30px;
+    width: 110px;
+    height: 110px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+  &.uploadbtn {
+    position: relative;
+    top: 70px;
+    left: 50px;
+  }
+`;
+const ErrorMessage = styled.p`
+  color: var(--color-primary);
+  margin-top: 10px;
+`;
 
 export default function ProfileSetting() {
+  const [profileImage, setProfileImage] = useState(defaultimg);
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const [userName, setUserName] = useState("");
   const [userNameErrorMsg, setUserNameErrorMsg] = useState("");
 
   const [nickName, setNickName] = useState("");
   const [nickNameErrorMsg, setNickNameErrorMsg] = useState("");
 
-  const handleUserNameChange = (e) => {
-    const newUserName = e.target.value;
-    setUserName(newUserName);
+  const [intro, setIntro] = useState("");
 
-    if (newUserName.length < 2 || newUserName.length > 10) {
-      setUserNameErrorMsg("사용자 이름은 2~10자 이내여야 합니다.");
+  const location = useLocation();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setProfileImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+      setSelectedImage(file);
+      uploadProfileImage(file);
     } else {
-      setUserNameErrorMsg("");
+      setProfileImage(defaultimg);
+      setSelectedImage(null);
     }
   };
+  const uploadProfileImage = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
 
+      const response = await customAxios.post("image/uploadfile", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+      console.log(response);
+      setSelectedImage(response.data.filename);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
   const validateNickName = (nickName) => {
     const nickNamePattern = /^[a-zA-Z0-9._]+$/;
@@ -72,13 +133,88 @@ export default function ProfileSetting() {
     }
   };
 
+  const handleNickNameBlur = async () => {
+    try {
+      const response = await customAxios.post(`user/accountnamevalid`, {
+        user: {
+          accountname: nickName,
+        },
+      });
+      const data = response.data;
+      if (data.message === "이미 가입된 계정ID 입니다.") {
+        setNickNameErrorMsg(data.message);
+        return;
+      }
+      console.log(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUserNameChange = (e) => {
+    const newUserName = e.target.value;
+    setUserName(newUserName);
+
+    if (newUserName.length < 2 || newUserName.length > 10) {
+      setUserNameErrorMsg("사용자 이름은 2~10자 이내여야 합니다.");
+    } else {
+      setUserNameErrorMsg("");
+    }
+  };
+
+  const handleIntroChange = (e) => {
+    const value = e.target.value;
+    setIntro(value);
+  };
+
+  const isFormValid = () => {
+    return (
+      validateNickName(nickName) &&
+      userName.length >= 2 &&
+      userName.length <= 10
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    const baseUrl = process.env.REACT_APP_BASE_URL;
+    e.preventDefault();
+    const user = {
+      user: {
+        username: userName,
+        email: location.state.email,
+        password: location.state.password,
+        accountname: nickName,
+        intro: intro,
+        image: `${baseUrl}/${selectedImage}`, // 예시) https://api.mandarin.weniv.co.kr/1641906557953.png
+      },
+    };
+
+    try {
+      const response = await customAxios.post(`user`, user);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ProfileContainer>
-    <ProfileForm>
-      <Title>프로필 설정</Title>
-      <Subtitle>나중에 언제든지 변경할 수 있습니다.</Subtitle>
+      <ProfileForm>
+        <Title>프로필 설정</Title>
+        <Subtitle>나중에 언제든지 변경할 수 있습니다.</Subtitle>
+        <ImgLabel htmlFor="file-input">
+          <Img className="defaultlion" src={profileImage} alt="기본 이미지" />
+          <Img className="uploadbtn" src={uploadfile} alt="업로드 버튼" />
+        </ImgLabel>
+        <ImgInput
+          type="file"
+          id="file-input"
+          accept="image/*"
+          onChange={handleImageChange}
+        ></ImgInput>
 
-      <UserInput inputId="registernickname" label="사용자 이름">
+        <UserInput inputId="registernickname" label="사용자 이름">
           <DataInput
             type="text"
             id="registernickname"
@@ -96,6 +232,7 @@ export default function ProfileSetting() {
             placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
             value={nickName}
             onChange={handleNickNameChange}
+            onBlur={handleNickNameBlur}
           ></DataInput>
           {nickNameErrorMsg && <ErrorMessage>{nickNameErrorMsg}</ErrorMessage>}
         </UserInput>
@@ -105,11 +242,19 @@ export default function ProfileSetting() {
             type="text"
             id="registerintro"
             placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+            value={intro}
+            onChange={handleIntroChange}
           ></DataInput>
         </UserInput>
 
-      <ProfileButton className="large">수수마켓 시작하기</ProfileButton>
-    </ProfileForm>
-  </ProfileContainer>
-);
+        <ProfileButton
+          className="large"
+          disabled={!isFormValid()}
+          onClick={handleSubmit}
+        >
+          수수마켓 시작하기
+        </ProfileButton>
+      </ProfileForm>
+    </ProfileContainer>
+  );
 }
