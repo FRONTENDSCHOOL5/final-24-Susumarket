cd399d29baf080b5aaf74702d9921495cb1a7c16
zkdk1241 <zkdk1241@naver.com>
2023-06-18 23:06:57 +0900

    fix: dataInput.jsx->DataInput.jsx로 파일명 변경

--- src/components/commons/dataInput/dataInput.jsx
+++ src/components/commons/dataInput/dataInput.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-
+// dataInput->DataInput으로 파일명 변경
 import { DataInputStyle } from "./InputStyle.js";
-//
+
+
 export default function DataInput() {
   return <DataInputStyle></DataInputStyle>;
 }
