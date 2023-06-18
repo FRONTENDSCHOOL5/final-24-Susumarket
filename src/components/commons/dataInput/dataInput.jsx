05a20bc21fa824a206d4a608321eb10f2b9cb302
zkdk1241 <zkdk1241@naver.com>
2023-06-19 00:12:38 +0900

    fix: DataInput.jsx 수정

--- src/components/commons/dataInput/dataInput.jsx
+++ src/components/commons/dataInput/dataInput.jsx
@@ -1,14 +1,14 @@
-import styled from "styled-components";
-
 import { DataInputStyle } from "./InputStyle.js";
-
+//
 export default function DataInput(props) {
   return (
     <DataInputStyle
       type={props.type}
+      id={props.id}
       placeholder={props.placeholder}
       value={props.value} // value prop 추가
-      onChange={props.onChange}
+      onChange={props.onChange} // onChange prop 추가
+      onBlur={props.onBlur}
     ></DataInputStyle>
   );
 }
