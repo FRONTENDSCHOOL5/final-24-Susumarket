a91adb58abd0ffe86efedbb9b16efbc0e8e1f87d
Yunjung324 <rachel324cc@naver.com>
2023-06-18 18:10:26 +0900

    Feat: 회원가입 ProfileSetting 사용자 이름, 계정 id 에러 메시지 출력 코드 추가

--- src/pages/signup/profileSetting/ProfileSetting.jsx
+++ src/pages/signup/profileSetting/ProfileSetting.jsx
@@ -38,38 +38,75 @@ const ProfileButton = styled(Button)`
 
 
 export default function ProfileSetting() {
+  const [userName, setUserName] = useState("");
+  const [userNameErrorMsg, setUserNameErrorMsg] = useState("");
+
+  const [nickName, setNickName] = useState("");
+  const [nickNameErrorMsg, setNickNameErrorMsg] = useState("");
+
+  const handleUserNameChange = (e) => {
+    const newUserName = e.target.value;
+    setUserName(newUserName);
+
+    if (newUserName.length < 2 || newUserName.length > 10) {
+      setUserNameErrorMsg("사용자 이름은 2~10자 이내여야 합니다.");
+    } else {
+      setUserNameErrorMsg("");
+    }
+  };
+
+
+  const validateNickName = (nickName) => {
+    const nickNamePattern = /^[a-zA-Z0-9._]+$/;
+    return nickNamePattern.test(nickName);
+  };
+
+  const handleNickNameChange = (e) => {
+    const value = e.target.value;
+    setNickName(value);
+
+    if (!validateNickName(value)) {
+      setNickNameErrorMsg("*영문, 숫자 밑줄 및 마침표만 사용할 수 있습니다.");
+    } else {
+      setNickNameErrorMsg("");
+    }
+  };
+
   return (
     <ProfileContainer>
     <ProfileForm>
       <Title>프로필 설정</Title>
       <Subtitle>나중에 언제든지 변경할 수 있습니다.</Subtitle>
 
-      <UserInput inputId="text" label="사용자 이름">
-        <DataInput
-          type="text"
-          id="nickname"
-          placeholder="2~10자 이내여야 합니다."
-        ></DataInput>
-      </UserInput>
+      <UserInput inputId="registernickname" label="사용자 이름">
+          <DataInput
+            type="text"
+            id="registernickname"
+            placeholder="2~10자 이내여야 합니다."
+            value={userName}
+            onChange={handleUserNameChange}
+          ></DataInput>
+          {userNameErrorMsg && <ErrorMessage>{userNameErrorMsg}</ErrorMessage>}
+        </UserInput>
 
-      <UserInput inputId="text" label="계정 ID">
-        <DataInput
-          type="text"
-          id="nickname"
-          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-          value={nickName}
-          onChange={handleNickNameChange}
-        ></DataInput>
-        {nickNameErrorMsg && <ErrorMessage>{nickNameErrorMsg}</ErrorMessage>}
-      </UserInput>
+        <UserInput inputId="registerid" label="계정 ID">
+          <DataInput
+            type="text"
+            id="registerid"
+            placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+            value={nickName}
+            onChange={handleNickNameChange}
+          ></DataInput>
+          {nickNameErrorMsg && <ErrorMessage>{nickNameErrorMsg}</ErrorMessage>}
+        </UserInput>
 
-      <UserInput inputId="text" label="소개">
-        <DataInput
-          type="text"
-          id="nickname"
-          placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
-        ></DataInput>
-      </UserInput>
+        <UserInput inputId="registerintro" label="소개">
+          <DataInput
+            type="text"
+            id="registerintro"
+            placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+          ></DataInput>
+        </UserInput>
 
       <ProfileButton className="large">수수마켓 시작하기</ProfileButton>
     </ProfileForm>
