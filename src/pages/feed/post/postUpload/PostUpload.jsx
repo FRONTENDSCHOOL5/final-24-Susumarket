49e9f9536ab278e37c5b276f08651388292296f0
Yunjung324 <rachel324cc@naver.com>
2023-06-19 22:51:58 +0900

    Feat: PostUpload 마크업 추가, 스타일 적용, textarea height 자동조절 코드 추가

--- src/pages/feed/post/postUpload/PostUpload.jsx
+++ src/pages/feed/post/postUpload/PostUpload.jsx
@@ -1,7 +1,83 @@
-import React from 'react'
+import UploadTopHeader from "../../../../components/commons/topHeader/UploadTopHeader";
+import imgUploadBtn from "../../../../img/upload-file.svg";
+import styled from "styled-components";
+import TopButton from "../../../../components/commons/topButton/TopButton";
+import { useRef } from "react";
+import { useCallback } from "react";
+
+const UpLoadButton = styled.button`
+  // top: 70%;
+  position: fixed;
+  bottom: 70px;
+  right: 40px;
+  width: 50px;
+  height: 50px;
+  background: url(${imgUploadBtn}) no-repeat;
+  background-position: 0 0;
+`;
+
+const UploadMain = styled.main`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ProfileImgLabel = styled.label``;
+
+const ProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+  background-color: var(--color-sub);
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+
+const TextArea = styled.textarea`
+  font-size: 15px;
+  width: calc(85% - 90px);
+  outline: none;
+  border: none;
+  resize: none;
+  margin-left: 20px;
+  margin-top: 35px;
+`;
+const PostImgLabel = styled.label`
+  margin-top: 10px;
+`;
+
+const PostImgInput = styled.input`
+  display: none;
+`;
 
 export default function PostUpload() {
+  const textRef = useRef();
+  const handleTextAreaHeight = useCallback(() => {
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, []);
+
   return (
-    <div>PostUpload</div>
-  )
+    <>
+      <UploadTopHeader disabled></UploadTopHeader>
+      <UploadMain>
+        <ProfileImgLabel></ProfileImgLabel>
+        <ProfileImg />
+        <TextArea
+          ref={textRef}
+          placeholder="게시글 입력하기..."
+          onInput={handleTextAreaHeight}
+          rows="1"
+        ></TextArea>
+        <PostImgLabel htmlFor="input-file"></PostImgLabel>
+        <PostImgInput
+          type="file"
+          multiple="multiple"
+          id="input-file"
+        ></PostImgInput>
+      </UploadMain>
+      <UpLoadButton disabled></UpLoadButton>
+      <TopButton></TopButton>
+    </>
+  );
 }
