6a069e9755604fecde3cc37ce5d1319da5101445
nastar <an1413@hs.ac.kr>
2023-06-19 17:58:00 +0900

    Feat: Search 페이지 마크업 및 스타일 구현추가

--- src/pages/feed/search/Search.jsx
+++ src/pages/feed/search/Search.jsx
@@ -1,6 +1,25 @@
+import styled from "styled-components";
 import React from "react";
-import "search.style.js";
+// import "search.style.js";
+import MenuBar from "../../../components/commons/menuBar/MenuBar";
+import SearchTopHeader from "../../../components/commons/topHeader/SearchTopHeader";
+import SearchList from "./SearchList";
+
+export const SearchWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  padding: 20px 16px;
+  overflow-y: scroll;
+`;
 
 export default function Search() {
-  return <div>Search</div>;
+  return (
+    <>
+      <SearchTopHeader />
+      <SearchWrapper>
+        <SearchList />
+      </SearchWrapper>
+      <MenuBar />
+    </>
+  );
 }
