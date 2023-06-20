c19779547781e87f6b8cfc168a4ec23f0f4b7ee0
nastar <an1413@hs.ac.kr>
2023-06-20 14:54:59 +0900

    Fix: search페이지가 처음 랜더링 될때 유저List 검색하는 문제 해결

--- src/pages/feed/search/Search.jsx
+++ src/pages/feed/search/Search.jsx
@@ -14,21 +14,42 @@ export default function Search() {
   };
   useEffect(() => {
     console.log(inputValue);
-
     // 키 확인하기 api 호출
-    customAxios
-      .get(`/user/searchuser/?keyword=${inputValue}`)
-      .then((response) => {
-        // 요청에 대한 응답 처리
+    const fetchData = async () => {
+      try {
+        // 키 확인하기 api 호출
+        const response = await customAxios.get(
+          `/user/searchuser/?keyword=${inputValue}`,
+        );
         console.log("response.data", response.data);
         setUserList(response.data);
         // userData 저장
-      })
-      .catch((error) => {
+      } catch (error) {
         // 에러 처리
         console.error();
-      });
+      }
+    };
+
+    // search페이지로 처음 이동할때도 검색하는 것 해결
+    if (inputValue !== "") {
+      fetchData();
+    }
   }, [inputValue]);
+
+  //   }
+  //   customAxios
+  //     .get(`/user/searchuser/?keyword=${inputValue}`)
+  //     .then((response) => {
+  //       // 요청에 대한 응답 처리
+  //       console.log("response.data", response.data);
+  //       setUserList(response.data);
+  //       // userData 저장
+  //     })
+  //     .catch((error) => {
+  //       // 에러 처리
+  //       console.error();
+  //     });
+  // }, [handleInputChange]);
 
   return (
     <>
