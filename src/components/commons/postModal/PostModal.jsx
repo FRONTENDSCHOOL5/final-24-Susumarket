978bb6d4293c8b0dadc01d97bca7a4b653e60a43
jongtae.nam <dark9737@gmail.com>
2023-06-20 16:28:34 +0900

    Fix: PostModal 공통 컴포넌트 backdrop 클릭 이벤트 수정

--- src/components/commons/postModal/PostModal.jsx
+++ src/components/commons/postModal/PostModal.jsx
@@ -13,7 +13,7 @@ const PostModal = ({ menuList }) => {
   // props로 isOpen과 onclose를 안받아도 됩니다.
   // menuList라는 props는 name과 Func로 이루어진 배열 안의 객체입니다. ex)[{name:"버튼이름"", func:버튼 클릭 시 실행 함수}]
   // name에는 버튼에 들어갈 text를 넣어주고, func 해당 버튼에서 동작할 함수를 넣어줍니다.
-  const { isOpenPostModal, setIsOpenPostModal } = useContext(ModalContext);
+  const { isOpenPostModal, setIsOpenPostModal, setIsOpenConfirmModal } = useContext(ModalContext);
   return (
     <>
       <BottomSheetModal isOpen={isOpenPostModal}>
@@ -25,7 +25,10 @@ const PostModal = ({ menuList }) => {
           )})}
         </BottomSheetUl>
       </BottomSheetModal>
-      <BottomSheetBackdrop  isOpen={isOpenPostModal} onClick={()=>setIsOpenPostModal(false)}/>
+      <BottomSheetBackdrop  isOpen={isOpenPostModal} onClick={()=>{
+        setIsOpenConfirmModal(false);
+        setIsOpenPostModal(false);
+        }}/>
     </>
   );
 };
