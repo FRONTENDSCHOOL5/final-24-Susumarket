5b1718224f58bf198f6134e538b8f383f757d989
jongtae.nam <dark9737@gmail.com>
2023-06-20 22:02:41 +0900

    Feat: ProfileProduct 컴포넌트 API 요청 및 데이터 UI 적용

--- src/components/units/profile/ProfileProduct/ProfileProduct.jsx
+++ src/components/units/profile/ProfileProduct/ProfileProduct.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   ProfileProductTitle,
   ProfileProductUl,
   ProfileProductWrapper,
 } from "./ProfileProduct.styles";
 import ProfileProductList from "./ProfileProductList";
-
+import { customAxios } from "../../../../library/customAxios";
+import {
+  PostNoneImg,
+  PostNoneText,
+  PostNoneWrapper,
+} from "../ProfilePost/ProfilePost.styles";
+import ProductNoneImgIcon from "../../../../img/symbol-logo-404.svg";
+import { useParams } from "react-router-dom";
 export default function ProfileProduct({
   onClickButton,
   settingPostModalProps,
   closeModal,
 }) {
+  const [productData, setProductData] = useState([]);
+  const [isNoneProductData, setIsNoneProductData] = useState(false);
+  const params = useParams();
+  const accountname = params.userId;
+  const fetchProdcutData = useCallback(async () => {
+    try {
+      const response = await customAxios.get(`product/${accountname}`);
+      setProductData(response.data.product);
+      if (response.data.product.length === 0) {
+        setIsNoneProductData(true);
+      } else {
+        setIsNoneProductData(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [accountname]);
+
+  useEffect(() => {
+    if (accountname) {
+      fetchProdcutData();
+    }
+  }, [accountname]);
   return (
     <ProfileProductWrapper>
       <ProfileProductTitle>판매 중인 상품</ProfileProductTitle>
-      <ProfileProductUl>
-        <ProfileProductList
-          onClickButton={onClickButton}
-          settingPostModalProps={settingPostModalProps}
-          closeModal={closeModal}
-        />
-      </ProfileProductUl>
+      {isNoneProductData ? (
+        <PostNoneWrapper>
+          <PostNoneImg src={ProductNoneImgIcon} alt="게시물 없음 아이콘" />
+          <PostNoneText>현재 판매 중인 상품이 없어요.</PostNoneText>
+        </PostNoneWrapper>
+      ) : (
+        <ProfileProductUl>
+          {productData &&
+            productData.map((product) => {
+              return (
+                <ProfileProductList
+                  key={product.id}
+                  onClickButton={onClickButton}
+                  settingPostModalProps={settingPostModalProps}
+                  closeModal={closeModal}
+                  productList={product}
+                  reFetchProdcutData={fetchProdcutData}
+                />
+              );
+            })}
+        </ProfileProductUl>
+      )}
     </ProfileProductWrapper>
   );
 }
