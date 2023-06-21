ac7475ec3192ca56e6ef9132b415513664f8d8d9
jongtae.nam <dark9737@gmail.com>
2023-06-21 13:09:27 +0900

    Chore: ProfilePost 컴포넌트 사용하지 않는 idx props 삭제

--- src/components/units/profile/ProfilePost/ProfilePost.jsx
+++ src/components/units/profile/ProfilePost/ProfilePost.jsx
@@ -88,7 +88,6 @@ export default function ProfilePost({
                 closeModal={closeModal}
                 reFetchPostData={fetchPostData}
                 post={post}
-                idx={idx}
               />
             );
           })}
