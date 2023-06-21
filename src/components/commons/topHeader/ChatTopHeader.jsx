0d907b298c28931c7bf4c9cdd6ad3a24b8f26991
jongtae.nam <dark9737@gmail.com>
2023-06-21 16:20:58 +0900

    Fix: TopHeader 공통 컴포넌트 h1 태그 추가 및 TopHeaderWrapper header 태그로 수정

--- src/components/commons/topHeader/ChatTopHeader.jsx
+++ src/components/commons/topHeader/ChatTopHeader.jsx
@@ -9,11 +9,12 @@ import {
   TopHeaderMoreIconImg,
   TopHeaderMoreBtn,
   TopHeaderLeft,
+  TopHeaderMainTitle,
 } from "./TopHeaderStyle.js";
 import { useNavigate } from "react-router-dom";
 import { ModalContext } from "../../../context/ModalContext";
 
-export default function ChatTopHeader({ usernameTitle }) {
+export default function ChatTopHeader({ usernameTitle, headerText }) {
   // 컨텍스트에 저장된 setIsOPenPostModal를 불러와서 모달창을 관리
   const { setIsOpenPostModal } = useContext(ModalContext);
   const navigation = useNavigate();
@@ -29,6 +30,7 @@ export default function ChatTopHeader({ usernameTitle }) {
   }
   return (
     <TopHeaderWrapper>
+      <TopHeaderMainTitle className="a11y-hidden">{headerText}</TopHeaderMainTitle>
       <TopHeaderLeft>
         <TopHeaderBackBtn onClick={() => onClickBack(`/chatList`)}>
           <TopHeaderBackIcon src={backIcon} alt="뒤로가기" />
--- src/components/commons/topHeader/ProfileEditTopHeader.jsx
+++ src/components/commons/topHeader/ProfileEditTopHeader.jsx
@@ -3,19 +3,27 @@ import backIcon from "../../../img/icon-arrow-left.svg";
 import Button from "../button/Button";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { TopHeaderWrapper, TopHeaderBackBtn, TopHeaderBackIcon } from "./TopHeaderStyle.js";
+import {
+  TopHeaderWrapper,
+  TopHeaderBackBtn,
+  TopHeaderBackIcon,
+  TopHeaderMainTitle,
+} from "./TopHeaderStyle.js";
 
-export default function ProfileTopEdit() {
+export default function ProfileTopEdit({ headerText }) {
   const { userId } = useParams();
   const navigation = useNavigate();
 
-   // 이전 페이지로 이동시키는 함수
+  // 이전 페이지로 이동시키는 함수
   function onClickBack(url) {
     navigation(url);
   }
 
   return (
     <TopHeaderWrapper>
+      <TopHeaderMainTitle className="a11y-hidden">
+        {headerText}
+      </TopHeaderMainTitle>
       <TopHeaderBackBtn onClick={() => onClickBack(`profile/${userId}`)}>
         <TopHeaderBackIcon src={backIcon} alt="뒤로가기" />
       </TopHeaderBackBtn>
