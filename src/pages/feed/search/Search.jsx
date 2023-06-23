83610fb4948aeb13c42b130af2038940b47a24f1
nastar <an1413@hs.ac.kr>
2023-06-23 16:10:02 +0900

    Refactor: lodash 라이브러리 대체 적용

--- src/pages/feed/search/Search.jsx
+++ src/pages/feed/search/Search.jsx
@@ -1,50 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { debounce } from "lodash";
 import { SearchWrapper } from "./search.style";
 import SearchTopHeader from "../../../components/commons/topHeader/SearchTopHeader";
 import SearchList from "./SearchList";
 import { customAxios } from "../../../library/customAxios";
-import { useDebounce } from "../../../library/useDebounce";
 
 export default function Search() {
   const [inputValue, setInputValue] = useState(""); //
   const [userList, setUserList] = useState([]); // 유저정보리스트
 
-  const debounceValue = useDebounce(inputValue);
-
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
-    console.log(inputValue);
+    handleSearch(inputValue);
   };
 
-  // 스페이스바, 엔터키 제외한 문자열 입력됐을 때 api 호출하기 위한 함수
-  useEffect(() => {
-    if (debounceValue.trim() === "") {
-      setUserList([]);
-    } else {
-      fetchData();
-    }
-    // 유저검색 api 호출
-    async function fetchData() {
+  const handleSearch = useCallback(
+    debounce(async (inputValue) => {
       try {
         const response = await customAxios.get(
-          `/user/searchuser/?keyword=${debounceValue}`,
+          `/user/searchuser/?keyword=${inputValue}`,
         );
         setUserList(response.data);
         console.log(response.data);
       } catch (error) {
         console.error(error);
       }
-    }
-  }, [debounceValue]);
+    }, 500),
+    [],
+  );
 
   return (
     <>
       <SearchTopHeader
-        value={debounceValue}
+        value={inputValue}
         handleInputChange={handleInputChange}
       />
       <SearchWrapper>
-        <SearchList inputValue={debounceValue} userList={userList} />
+        <SearchList inputValue={inputValue} userList={userList} />
       </SearchWrapper>
     </>
   );
