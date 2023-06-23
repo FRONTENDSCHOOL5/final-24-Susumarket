8b92b3282b0b6035765fd92fcae94514e0fbb5dc
jongtae.nam <dark9737@gmail.com>
2023-06-23 10:50:12 +0900

    Fix: ProfilePost 컴포넌트 스타일 수정 및 ProfilePostGallery 컴포넌트 이미지가 없을 경우 나타나는 화면 추가

--- src/components/units/profile/ProfilePost/ProfilePostGallery.jsx
+++ src/components/units/profile/ProfilePost/ProfilePostGallery.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { ProfilePostGalleryUl } from "./ProfilePost.styles";
+import {
+  PostNoneImg,
+  PostNoneText,
+  PostNoneWrapper,
+  ProfilePostGalleryUl,
+} from "./ProfilePost.styles";
 import ProfilePostGalleryList from "./ProfilePostGalleryList";
+import PostNoneImgIcon from "../../../../img/symbol-logo-404.svg";
 export default function ProfilePostGallery({ postData }) {
   return (
-    <ProfilePostGalleryUl>
-      {postData.map((post) => {
-        return <ProfilePostGalleryList key={post.id} post={post} />;
-      })}
-    </ProfilePostGalleryUl>
+    <>
+      {postData.find((el) => el.image.length > 0) ? (
+        <ProfilePostGalleryUl>
+          {postData.map((post) => {
+            return (
+              <>
+                {post.image && (
+                  <ProfilePostGalleryList key={post.id} post={post} />
+                )}
+              </>
+            );
+          })}
+        </ProfilePostGalleryUl>
+      ) : (
+        <PostNoneWrapper>
+          <PostNoneImg src={PostNoneImgIcon} alt="게시물 없음 아이콘" />
+          <PostNoneText>현재 등록된 이미지 게시물이 없어요.</PostNoneText>
+        </PostNoneWrapper>
+      )}
+    </>
   );
 }
