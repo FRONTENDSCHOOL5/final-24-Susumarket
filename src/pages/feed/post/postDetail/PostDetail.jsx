fb5021be137472caaf71ad7940d9228ba401abdf
Yunjung324 <rachel324cc@naver.com>
2023-06-24 15:09:01 +0900

    Feat: 게시물 내 게시글과 이미지 api 불러오고 화면에 출력

--- src/pages/feed/post/postDetail/PostDetail.jsx
+++ src/pages/feed/post/postDetail/PostDetail.jsx
@@ -58,6 +58,25 @@ export default function PostDetail() {
     }
   };
   fetchProfile();
+
+    // 지속적인 실행 막기 위해 useEffect 사용
+    useEffect(() => {
+      fetchProfile();
+      fetchPostDetail();
+    }, []);
+  
+    // api에서 업로드 된 post 내용과 이미지 불러오기
+    const fetchPostDetail = async () => {
+      try {
+        const response = await customAxios.get(`post/${postId}`);
+        setPostContent(response.data.post.content);
+        setPostImage(response.data.post.image);
+        setImgArray(response.data.post.image.split(","));
+        console.log("이미지 : ", response.data.post.image.split(","));
+      } catch (error) {
+        console.error(error);
+      }
+    };
   return (
     <>
       <NewTopHeader left="back" right="more"></NewTopHeader>
@@ -68,6 +87,41 @@ export default function PostDetail() {
           <AccountName>@ {accountname}</AccountName>
         </UserInfo>
         <UserButton></UserButton>
+        <PostWrapper>
+        <PostContent>{postcontent}</PostContent>
+        {imgArray.length > 0 ? (
+          <ProfilePostImgWrapper>
+            <ProfilePostImgUl ref={ImgUlRef}>
+              {imgArray.map((image, idx) => {
+                return (
+                  <ProfilePostImgLi key={image + idx}>
+                    <ProfilePostImg src={image} alt="포스트 이미지" />
+                  </ProfilePostImgLi>
+                );
+              })}
+            </ProfilePostImgUl>
+
+            <ProfilePostImgBtnUl>
+              {imgArray.map((image, idx) => {
+                return (
+                  <ProfilePostImgBtnLi key={image + idx}>
+                    {imgArray.length > 1 && (
+                      <ProfilePostImgBtn
+                        className={activeButton === idx ? "active" : ""}
+                        onClick={(e) => onClickSliderBtn(e, idx)}
+                      >
+                        <ProfilePostButtonSpan className="a11y-hidden">
+                          이미지 슬라이드 버튼
+                        </ProfilePostButtonSpan>
+                      </ProfilePostImgBtn>
+                    )}
+                  </ProfilePostImgBtnLi>
+                );
+              })}
+            </ProfilePostImgBtnUl>
+          </ProfilePostImgWrapper>
+        ) : null}
+      </PostWrapper>
       </UserWrapper>
     </>
   );
