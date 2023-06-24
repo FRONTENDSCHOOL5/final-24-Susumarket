0144f109862bde6c915348fb0e25dc0f8b188bd0
jongtae.nam <dark9737@gmail.com>
2023-06-25 00:40:50 +0900

    Fix: ProductDetail 페이지, ProductEdit 페이지, ProductUpload 페이지 수정

--- src/pages/feed/product/productDetail/ProductDetail.jsx
+++ src/pages/feed/product/productDetail/ProductDetail.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import UserInput from "../../../../components/commons/dataInput/UserInput"
-import DataInput from "../../../../components/commons/dataInput/DataInput"
-import defaultimg from "../../../../img/ProfileImg.svg"
-import uploadfile from "../../../../img/upload-file.svg";
-import NewTopHeader from '../../../../components/commons/newTopHeader/NewTopHeader';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NewTopHeader from "../../../../components/commons/newTopHeader/NewTopHeader";
 import {
-  txtContainer, Cont, Container, Form, Img, ImgInput, ImgLabel, ImgContainer, ImgTopLabel, txt
+  Cont,
+  Container,
+  Img,
+  ImgInput,
+  ImgContainer,
+  ImgTopLabel,
 } from "./productDetail.style.js";
-import { customAxios } from '../../../../library/customAxios'
-// import { useLocation } from "react-router-dom";
+import noImg from "../../../../img/symbol-logo-404.svg";
+import { customAxios } from "../../../../library/customAxios";
+
 
 export default function ProductDetail() {
-  const [itemName, setItemName] = useState('');
-  const [price, setPrice] = useState('');
-  const [link, setLink] = useState('');
-  const [itemImage, setItemImage] = useState('');
-  const [images, setImages] = useState([]);
-  const navigate = useNavigate();
-  // const location = useLocation();
+  const [itemName, setItemName] = useState("");
+  const [price, setPrice] = useState("");
+  const [link, setLink] = useState("");
+  const [itemImage, setItemImage] = useState("");
+
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const params = useParams();
 
   // 이미지 로딩
   useEffect(() => {
     const loadProfileImage = async () => {
-      const url = `${baseUrl}product/detail/${params.productId}`
+      const url = `${baseUrl}product/detail/${params.productId}`;
       try {
         const response = await customAxios.get(url);
         setItemImage(response.data.product.itemImage);
@@ -34,7 +34,6 @@ export default function ProductDetail() {
         setLink(response.data.product.link);
         const data = response.data;
         console.log(data);
-
       } catch (error) {
         console.error(error);
       }
@@ -44,19 +43,20 @@ export default function ProductDetail() {
 
   return (
     <Container>
-      <NewTopHeader left={"back"} right={""} ></NewTopHeader>
+      <NewTopHeader left={"back"} right={""}></NewTopHeader>
       <Cont>
         <ImgContainer>
           <ImgTopLabel>{itemName}</ImgTopLabel>
           <Img
             className="default"
             src={itemImage}
+            onError={(e) => (e.target.src = noImg)}
           />
           <ImgInput
             type="file"
             id="file-input"
             accept="image/*"
-          // onChange={handleImageChange}
+            // onChange={handleImageChange}
           ></ImgInput>
         </ImgContainer>
 
@@ -71,10 +71,7 @@ export default function ProductDetail() {
           <txt> 판매링크: </txt>
           <txt> {link}</txt>
         </txtContainer>
-
       </Cont>
-    </Container >
-
-  )
+    </Container>
+  );
 }
-
