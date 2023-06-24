1d252c28c8e70f37b4115b0bb3084048d9ad932d
jongtae.nam <dark9737@gmail.com>
2023-06-25 00:42:46 +0900

    Fix: Follower 페이지, Following 페이지 수정

--- src/pages/profile/follow/followers/Followers.jsx
+++ src/pages/profile/follow/followers/Followers.jsx
@@ -2,28 +2,17 @@ import React from "react";
 import FollowersTopHeader from "../../../../components/commons/topHeader/FollowersTopHeader";
 import { FollowersWrapper } from "./followers.style";
 import FollowerList from "./FollowerList";
-import { useEffect, useState, useContext } from "react";
-import { UserContext } from "../../../../context/UserContext";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FollowerListUl } from "./followerList.style";
 import followerAPI from "./followerAPI";
-import { customAxios } from "../../../../library/customAxios";
+import MenuBar from "../../../../components/commons/menuBar/MenuBar";
 
 export default function Followers() {
-  const [myFollowerList, setMyFollowerList] = useState([]); // 팔로워들의 데이터
   const [followerData, setFollowerData] = useState([]);
-  const { account } = useContext(UserContext);
+
   const { userId } = useParams();
 
-  // UserData 받아오기
-  useEffect(() => {
-    async function fetchUserData() {
-      const response = await customAxios.get(`profile/${account}/following`);
-      console.log("response.data", response.data);
-      setMyFollowerList(response.data);
-    }
-    fetchUserData();
-  }, []);
 
   // 팔로워 데이터 호출
   useEffect(() => {
@@ -50,8 +39,7 @@ export default function Followers() {
           {followerData.map((follower) => {
             return (
               <FollowerList
-                account={account}
-                myFollowerList={myFollowerList}
+                account={userId}
                 follower={follower}
                 key={follower._id}
               />
@@ -59,6 +47,7 @@ export default function Followers() {
           })}
         </FollowerListUl>
       </FollowersWrapper>
+      <MenuBar />
     </>
   );
 }
