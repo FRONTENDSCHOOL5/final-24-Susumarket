2f1c9528879ee231c1f583bdfce99f5a80775114
nastar <an1413@hs.ac.kr>
2023-06-26 01:14:06 +0900

    Feat: NotFound페이지 NewTopHeader추가

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -8,11 +8,19 @@ import {
 import Button from "../../components/commons/button/Button";
 import Svg404 from "../../img/symbol-logo-404.svg";
 import { useNavigate } from "react-router-dom";
+import NewTopHeader from "../../components/commons/newTopHeader/NewTopHeader";
 
 export default function NotFound() {
   const navigate = useNavigate();
   return (
     <>
+      <NewTopHeader
+        title={"질못된 경로의 페이지"}
+        text={"잘못된 경로의 페이지입니다."}
+        left={"back"}
+        leftSide={true}
+        middle={"text"}
+      />
       <NotFoundWrapper>
         <NotFoundTitle>
           <NotFoundImage src={Svg404} alt="" />
