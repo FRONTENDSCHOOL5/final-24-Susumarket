6ec37c8f3be2ec15cbfc1812e4be971829eb875f
nastar <an1413@hs.ac.kr>
2023-06-26 11:27:30 +0900

    Fix: follow 리스트 페이지의 마지막리스트 마진값 추가

--- src/pages/profile/follow/followers/followers.style.js
+++ src/pages/profile/follow/followers/followers.style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const FollowersWrapper = styled.main`
-  height: calc(100vh - 122px);
   padding: 24px 16px;
   display: flex;
   flex-direction: column;
