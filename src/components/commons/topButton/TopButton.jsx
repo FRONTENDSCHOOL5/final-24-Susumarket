1af43063de9f704b4bf640012ef9d0d55c92015f
jongtae.nam <dark9737@gmail.com>
2023-06-27 10:26:15 +0900

    Fix: TopButton 스타일 분리 및 수정

--- src/components/commons/topButton/TopButton.jsx
+++ src/components/commons/topButton/TopButton.jsx
@@ -1,28 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
-import styled from "styled-components";
-import img from "../../../img/TopButton.svg";
+import { throttle } from "lodash";
+import { Button } from "./topButton.style";
 
 const TopButton = () => {
   const [showBtn, setShowBtn] = useState(false);
   const btnRef = useRef();
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 500) {
-        setShowBtn(true);
-      } else {
-        if (btnRef.current) {
-          btnRef.current.classList.add("inactive");
-          setTimeout(() => {
-            setShowBtn(false);
-          }, 300);
-        }
+
+  const handleScroll = throttle(() => {
+    if (window.scrollY > 500) {
+      setShowBtn(true);
+    } else {
+      if (btnRef.current) {
+        btnRef.current.classList.add("inactive");
+        setTimeout(() => {
+          setShowBtn(false);
+        }, 300);
       }
-    };
+    }
+  },200);
+
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  
   const MoveTop = () => {
     window.scroll({
       top: 0,
@@ -30,38 +33,13 @@ const TopButton = () => {
     });
   };
 
-  return <>{showBtn && <Button onClick={MoveTop} ref={btnRef}></Button>}</>;
+  return (
+    <>
+      {showBtn && (
+        <Button type="button" onClick={MoveTop} ref={btnRef}></Button>
+      )}
+    </>
+  );
 };
-
-const Button = styled.button`
-  position: fixed;
-  bottom: 100px;
-  right: 50px;
-  height: 36px;
-  width: 36px;
-  background-image: url(${img});
-  background-color: rgba(0, 0, 0, 0);
-  animation: fadeIn 0.5s;
-  &.inactive {
-    animation: fadeOut 0.5s;
-  }
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  @keyframes fadeOut {
-    from {
-      opacity: 1;
-    }
-    to {
-      opacity: 0;
-    }
-  }
-`;
 
 export default TopButton;
---
+++ src/components/commons/topButton/topButton.style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import img from "../../../img/TopButton.svg";
+
+export const Button = styled.button`
+  position: fixed;
+  bottom: 100px;
+  right: 30px;
+  height: 36px;
+  width: 36px;
+  background-image: url(${img});
+  background-color: rgba(0, 0, 0, 0);
+  animation: fadeIn 0.5s;
+  @media screen and (max-width: 500px) {
+    right: 10px;
+    bottom: 80px;
+  }
+  &.inactive {
+    animation: fadeOut 0.5s;
+  }
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+`;
