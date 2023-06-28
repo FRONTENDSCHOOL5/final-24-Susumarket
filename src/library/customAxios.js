ad581d725c1f7efd8d7d9a36528f3ec3200a54cd
jongtae.nam <dark9737@gmail.com>
2023-06-28 23:38:46 +0900

    Fix: customAxios baseUr interceptorsl 안에서  선언 되게 변경

--- src/library/customAxios.js
+++ src/library/customAxios.js
@@ -1,25 +1,12 @@
 import axios from "axios";
 
-export const customAxios = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-  headers: {
-    "Content-Type": "application/json", // default 값
-  },
-});
+export const customAxios = axios.create();
 
-// axios 요청전 토큰을 불러와서 적용시켜줌
-// 초기 로그인시 customAxios가 호출됨 이때는 token 값 없이 customAxios가 설정됨 
-// 이후 새로고침을 하지않으면 customAxios 설정은 바뀌지 않고 token값이 없이 실행되게됨
-// 이를 수정하려고 아래와 같은 로직 사용
-// customAxios에서 api 요청전 실행되는 함수로 매 API 호출전 accessToken를 새로 초기화 해주도록 처리
-// https://velog.io/@liankim/customizing-axios 관련 레퍼런스
 customAxios.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem("accessToken");
-    config.headers.Authorization = accessToken
-      ? `Bearer ${accessToken}`
-      : "";
-
+    config.baseURL = process.env.REACT_APP_BASE_URL;
+    config.headers.Authorization = accessToken ? `Bearer ${accessToken}` : "";
     return config;
   },
   (error) => {
@@ -27,4 +14,3 @@ customAxios.interceptors.request.use(
     return Promise.reject(error);
   },
 );
-
