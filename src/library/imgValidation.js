ed8b994af071a2641dad1623b087cc50e09ea094
jongtae.nam <dark9737@gmail.com>
2023-06-28 23:38:04 +0900

    Fix: imgValidation 이미지 확장자 유효성 검사 오류 수정

--- src/library/imgValidation.js
+++ src/library/imgValidation.js
@@ -1,4 +1,5 @@
 export const imgValidation = (file) => {
+  const reg = /(.*?)\.(jpg|jpeg|png|gif|bmp|tif|heic)$/;
   // 파일 확인
   if (!file) {
     return false;
@@ -10,13 +11,7 @@ export const imgValidation = (file) => {
   }
   // 이미지 지원 형식 확인
   if (
-    !file.name.includes("png") &&
-    !file.name.includes("jpg") &&
-    !file.name.includes("jpeg") &&
-    !file.name.includes("bmp") &&
-    !file.name.includes("tif") &&
-    !file.name.includes("heic") &&
-    !file.name.includes("gif")
+    !reg.test(file.name)
   ) {
     alert(
       "이미지 형식을 확인해 주세요!\n(지원형식 : .jpg,.gif, .png,.jpeg, .bmp,.tif, *.heic)"
@@ -26,3 +21,4 @@ export const imgValidation = (file) => {
   // 모두 만족 한다면 true 반환
   return true;
 };
+
