80800290fc650b0eda61ba1c9af32006f411ba9f
jongtae.nam <dark9737@gmail.com>
2023-07-22 20:27:44 +0900

    Feat: Drawing 컴포넌트 container, presenter 패턴으로 파일 분리

--- src/pages/drawing/Drawing.container.jsx
+++ src/pages/drawing/Drawing.container.jsx
@@ -1,27 +1,8 @@
 import { isMobile } from "react-device-detect";
 import React, { useContext, useEffect, useRef, useState } from "react";
-import {
-  DrawingBtn,
-  DrawingBtns,
-  DrawingCanvers,
-  DrawingWrapper,
-  InputColor,
-  InputLabel,
-  InputLineWidth,
-  InputText,
-  InputUpload,
-  PalettColorBtn,
-  PalettColorSpan,
-  PaletteTitle,
-  PaletteWrapper,
-  TopHedaerWrapper,
-  UploadLabel,
-} from "./darwing.styles";
 import { useNavigate } from "react-router-dom";
-import MenuBar from "../../components/commons/menuBar/MenuBar";
-import PostModal from "../../components/commons/postModal/PostModal";
 import { ModalContext } from "../../context/ModalContext";
-import NewTopHeader from "../../components/commons/newTopHeader/NewTopHeader";
+import DrawingUI from "./Drawing.presenter";
 const colorData = [
   "#ff0000",
   "#ff8c00",
@@ -171,129 +152,29 @@ export default function Drawing() {
   }
 
   return (
-    <>
-      <TopHedaerWrapper style={{width:"100%", minWidth:"1200px"}}>
-        {" "}
-        <NewTopHeader
-          left={"back"}
-          middle={"text"}
-          text={"수수마켓 캔버스"}
-          right={"more"}
-          title={"캔버스"}
-          onClickButton={() => {
-            setIsOpenPostModal(true);
-          }}
-        />
-      </TopHedaerWrapper>
-
-      <DrawingWrapper>
-        <PaletteWrapper>
-          <PaletteTitle className="a11y-hidden">팔레트</PaletteTitle>
-          <InputLabel htmlFor="input-color" className="a11y-hidden">
-            색상 선택
-          </InputLabel>
-          <InputColor
-            type="color"
-            value={colorCode}
-            onChange={onColorChange}
-            id="input-color"
-          />
-          {colorData.map((color) => {
-            return (
-              <PalettColorBtn
-                key={color}
-                colors={color}
-                onClick={() => onColorClick(color)}
-              >
-                <PalettColorSpan className="a11y-hidden">
-                  {color + "색상"}
-                </PalettColorSpan>
-              </PalettColorBtn>
-            );
-          })}
-        </PaletteWrapper>
-        <DrawingCanvers
-          ref={canversRef}
-          onMouseMove={onMove}
-          onMouseDown={startPainting}
-          onMouseUp={cancelPainting}
-          onMouseLeave={cancelPainting}
-          onClick={onCanvasClick}
-          onDoubleClick={onDoubleClick}
-        ></DrawingCanvers>
-        <DrawingBtns>
-          <InputLabel htmlFor="input-lineWidth" className="a11y-hidden">
-            선 넓이
-          </InputLabel>
-          <InputLineWidth
-            id="input-lineWidth"
-            type="range"
-            onChange={onLineWidthChange}
-            min="1"
-            max="10"
-            value={lineWidth}
-            step="0.1"
-          />
-          <DrawingBtn
-            type="button"
-            className="medium"
-            active={true}
-            onClick={onModeClick}
-          >
-            {isFilling ? "Draw" : "Fill"}
-          </DrawingBtn>
-          <DrawingBtn
-            type="button"
-            className="medium"
-            active={true}
-            onClick={onDestroyClick}
-          >
-            Destroy
-          </DrawingBtn>
-          <DrawingBtn
-            type="button"
-            className="medium"
-            active={true}
-            onClick={onEraserClick}
-          >
-            Erase
-          </DrawingBtn>
-          <DrawingBtn
-            type="button"
-            className="medium"
-            active={true}
-            onClick={onSaveClick}
-          >
-            Save image
-          </DrawingBtn>
-          <InputText
-            value={text}
-            onChange={onChangeText}
-            placeholder="Add Text"
-          />
-          <UploadLabel className="medium" active={true} htmlFor="input-file">
-            Add Photo
-          </UploadLabel>
-          <InputUpload
-            type="file"
-            accept="image/*"
-            id="input-file"
-            onChange={onFileChange}
-          />
-        </DrawingBtns>
-      </DrawingWrapper>
-      <MenuBar />
-      <PostModal
-        menuList={[
-          {
-            name: "상품 등록 페이지로 이동",
-            func: () => {
-              setIsOpenPostModal(false);
-              navigate("/product/upload");
-            },
-          },
-        ]}
-      />
-    </>
+    <DrawingUI
+      colorCode={colorCode}
+      colorData={colorData}
+      text={text}
+      lineWidth={lineWidth}
+      isFilling={isFilling}
+      canversRef={canversRef}
+      setIsOpenPostModal={setIsOpenPostModal}
+      onChangeText={onChangeText}
+      onMove={onMove}
+      startPainting={startPainting}
+      cancelPainting={cancelPainting}
+      onLineWidthChange={onLineWidthChange}
+      onColorChange={onColorChange}
+      onColorClick={onColorClick}
+      onModeClick={onModeClick}
+      onCanvasClick={onCanvasClick}
+      onDestroyClick={onDestroyClick}
+      onEraserClick={onEraserClick}
+      onSaveClick={onSaveClick}
+      onDoubleClick={onDoubleClick}
+      onFileChange={onFileChange}
+      navigate={navigate}
+    />
   );
 }
---
+++ src/pages/drawing/Drawing.presenter.jsx
@@ -0,0 +1,172 @@
+import React from "react";
+import {
+  DrawingBtn,
+  DrawingBtns,
+  DrawingCanvers,
+  DrawingWrapper,
+  InputColor,
+  InputLabel,
+  InputLineWidth,
+  InputText,
+  InputUpload,
+  PalettColorBtn,
+  PalettColorSpan,
+  PaletteTitle,
+  PaletteWrapper,
+  TopHedaerWrapper,
+  UploadLabel,
+} from "./darwing.styles";
+import NewTopHeader from "../../components/commons/newTopHeader/NewTopHeader";
+import MenuBar from "../../components/commons/menuBar/MenuBar";
+import PostModal from "../../components/commons/postModal/PostModal";
+
+export default function DrawingUI({
+  colorCode,
+  colorData,
+  text,
+  lineWidth,
+  isFilling,
+  setIsOpenPostModal,
+  canversRef,
+  onChangeText,
+  onMove,
+  startPainting,
+  cancelPainting,
+  onLineWidthChange,
+  onColorChange,
+  onColorClick,
+  onModeClick,
+  onCanvasClick,
+  onDestroyClick,
+  onEraserClick,
+  onSaveClick,
+  onDoubleClick,
+  onFileChange,
+  navigate,
+}) {
+  return (
+    <>
+      <TopHedaerWrapper style={{ width: "100%", minWidth: "1200px" }}>
+        <NewTopHeader
+          left={"back"}
+          middle={"text"}
+          text={"수수마켓 캔버스"}
+          right={"more"}
+          title={"캔버스"}
+          onClickButton={() => {
+            setIsOpenPostModal(true);
+          }}
+        />
+      </TopHedaerWrapper>
+
+      <DrawingWrapper>
+        <PaletteWrapper>
+          <PaletteTitle className="a11y-hidden">팔레트</PaletteTitle>
+          <InputLabel htmlFor="input-color" className="a11y-hidden">
+            색상 선택
+          </InputLabel>
+          <InputColor
+            type="color"
+            value={colorCode}
+            onChange={onColorChange}
+            id="input-color"
+          />
+          {colorData.map((color) => {
+            return (
+              <PalettColorBtn
+                key={color}
+                colors={color}
+                onClick={() => onColorClick(color)}
+              >
+                <PalettColorSpan className="a11y-hidden">
+                  {color + "색상"}
+                </PalettColorSpan>
+              </PalettColorBtn>
+            );
+          })}
+        </PaletteWrapper>
+        <DrawingCanvers
+          ref={canversRef}
+          onMouseMove={onMove}
+          onMouseDown={startPainting}
+          onMouseUp={cancelPainting}
+          onMouseLeave={cancelPainting}
+          onClick={onCanvasClick}
+          onDoubleClick={onDoubleClick}
+        ></DrawingCanvers>
+        <DrawingBtns>
+          <InputLabel htmlFor="input-lineWidth" className="a11y-hidden">
+            선 넓이
+          </InputLabel>
+          <InputLineWidth
+            id="input-lineWidth"
+            type="range"
+            onChange={onLineWidthChange}
+            min="1"
+            max="10"
+            value={lineWidth}
+            step="0.1"
+          />
+          <DrawingBtn
+            type="button"
+            className="medium"
+            active={true}
+            onClick={onModeClick}
+          >
+            {isFilling ? "Draw" : "Fill"}
+          </DrawingBtn>
+          <DrawingBtn
+            type="button"
+            className="medium"
+            active={true}
+            onClick={onDestroyClick}
+          >
+            Destroy
+          </DrawingBtn>
+          <DrawingBtn
+            type="button"
+            className="medium"
+            active={true}
+            onClick={onEraserClick}
+          >
+            Erase
+          </DrawingBtn>
+          <DrawingBtn
+            type="button"
+            className="medium"
+            active={true}
+            onClick={onSaveClick}
+          >
+            Save image
+          </DrawingBtn>
+          <InputText
+            value={text}
+            onChange={onChangeText}
+            placeholder="Add Text"
+          />
+          <UploadLabel className="medium" active={true} htmlFor="input-file">
+            Add Photo
+          </UploadLabel>
+          <InputUpload
+            type="file"
+            accept="image/*"
+            id="input-file"
+            onChange={onFileChange}
+          />
+        </DrawingBtns>
+      </DrawingWrapper>
+      <MenuBar />
+      <PostModal
+        menuList={[
+          {
+            name: "상품 등록 페이지로 이동",
+            func: () => {
+              setIsOpenPostModal(false);
+              navigate("/product/upload");
+            },
+          },
+        ]}
+      />
+    </>
+  );
+}
