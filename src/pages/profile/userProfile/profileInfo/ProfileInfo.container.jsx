deeb5e3ad2210826e01a9a9a3a98c412e9c5ee0d
jongtae.nam <dark9737@gmail.com>
2023-07-24 11:46:45 +0900

    Feat: ProfileInfo followAPI, unFollowAPI 적용 및 try catch문 수정

--- src/pages/profile/userProfile/profileInfo/ProfileInfo.container.jsx
+++ src/pages/profile/userProfile/profileInfo/ProfileInfo.container.jsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { customAxios } from "../../../../library/customAxios";
 import { AccountContext } from "../../../../context/AccountContext";
 import ProfileInfoUI from "./ProfileInfo.presenter";
+import { followAPI, unFollowAPI } from "../../../../API/profileAPI";
 
 export default function ProfileInfo({ userData }) {
   const [isfollow, setIsfollow] = useState(userData.isfollow);
@@ -15,29 +15,20 @@ export default function ProfileInfo({ userData }) {
     navigate(url);
   }
 
-  // 좋은 방법이 아님 추후 수정예정
   useEffect(() => {
     setFollowCount(userData.followerCount);
   }, [userData]);
 
   const onClickFollow = useCallback(async () => {
-    try {
-      await customAxios.post(`profile/${userAccountname}/follow`);
+      await followAPI(userAccountname);
       setIsfollow(true);
       setFollowCount((prev) => prev + 1);
-    } catch (error) {
-      console.log(error);
-    }
   }, [isfollow, followCount]);
 
   const onClickUnfollow = useCallback(async () => {
-    try {
-      await customAxios.delete(`profile/${userAccountname}/unfollow`);
+      await unFollowAPI(userAccountname);
       setIsfollow(false);
       setFollowCount((prev) => prev - 1);
-    } catch (error) {
-      console.log(error);
-    }
   }, [isfollow, followCount]);
 
   return (
