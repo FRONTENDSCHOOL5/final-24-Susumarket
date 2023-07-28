e7e8a30e7f5d75308589b532a66c8da5f44685b9
Yunjung324 <rachel324cc@naver.com>
2023-07-28 13:17:31 +0900

    Refactoring: ProfileSetting 페이지 container, presenter 구조 변경 후 기존 파일 삭제

--- src/pages/signup/profileSetting/ProfileSetting.container.jsx
+++ src/pages/signup/profileSetting/ProfileSetting.container.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useCallback } from "react";
-import UserInput from "../../../components/commons/dataInput/UserInput";
-import DataInput from "../../../components/commons/dataInput/DataInput";
 import defaultimg from "../../../img/ProfileImg.svg";
-import uploadfile from "../../../img/upload-file.svg";
 import { useLocation } from "react-router-dom";
-import ErrorMessage from "../../../components/commons/errorMessage/ErrorMessage";
 import { useNavigate } from "react-router-dom";
-import {
-  ProfileContainer,
-  ProfileForm,
-  Title,
-  Subtitle,
-  ProfileButton,
-  ImgLabel,
-  ImgInput,
-  Img,
-} from "./profileSetting.style";
 import { accountValidationAPI } from "../../../API/validationAPI";
 import { signupAPI } from "../../../API/signupAPI";
 import { imgUploadAPI } from "../../../API/imgUploadAPI";
+import ProfileSettingUI from "./ProfileSetting.presenter";
 
-export default function ProfileSetting() {
+const ProfileSettingContainer = () => {
   const [profileImage, setProfileImage] = useState(defaultimg);
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -166,64 +153,23 @@ export default function ProfileSetting() {
   };
 
   return (
-    <ProfileContainer>
-      <ProfileForm>
-        <Title>프로필 설정</Title>
-        <Subtitle>나중에 언제든지 변경할 수 있습니다.</Subtitle>
-        <ImgLabel htmlFor="file-input">
-          <Img className="defaultlion" src={profileImage} alt="기본 이미지" />
-          <Img className="uploadbtn" src={uploadfile} alt="업로드 버튼" />
-        </ImgLabel>
-        <ImgInput
-          type="file"
-          id="file-input"
-          accept="image/*"
-          onChange={handleImageChange}
-        ></ImgInput>
-
-        <UserInput inputId="registernickname" label="사용자 이름">
-          <DataInput
-            type="text"
-            id="registernickname"
-            placeholder="2~10자 이내여야 합니다."
-            value={userName}
-            onChange={handleUserNameChange}
-          ></DataInput>
-          {userNameErrorMsg && <ErrorMessage>{userNameErrorMsg}</ErrorMessage>}
-        </UserInput>
-
-        <UserInput inputId="registerid" label="계정 ID">
-          <DataInput
-            type="text"
-            id="registerid"
-            placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-            value={nickName}
-            onChange={handleNickNameChange}
-            onBlur={handleNickNameBlur}
-          ></DataInput>
-          {nickNameErrorMsg && <ErrorMessage>{nickNameErrorMsg}</ErrorMessage>}
-        </UserInput>
-
-        <UserInput inputId="registerintro" label="소개">
-          <DataInput
-            type="text"
-            id="registerintro"
-            placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
-            value={intro}
-            onChange={handleIntroChange}
-          ></DataInput>
-        </UserInput>
-
-        <ProfileButton
-          className="large"
-          disabled={
-            !isFormValid() || nickNameErrorMsg === "이미 가입된 계정ID 입니다."
-          }
-          onClick={handleSubmit}
-        >
-          수수마켓 시작하기
-        </ProfileButton>
-      </ProfileForm>
-    </ProfileContainer>
+    <ProfileSettingUI
+      profileImage={profileImage}
+      userName={userName}
+      userNameErrorMsg={userNameErrorMsg}
+      nickName={nickName}
+      nickNameErrorMsg={nickNameErrorMsg}
+      intro={intro}
+      handleImageChange={handleImageChange}
+      handleNickNameChange={handleNickNameChange}
+      handleNickNameBlur={handleNickNameBlur}
+      handleUserNameChange={handleUserNameChange}
+      handleIntroChange={handleIntroChange}
+      isFormValid={isFormValid}
+      handleSubmit={handleSubmit}
+      onClickNextPage={onClickNextPage}
+    />
   );
-}
+};
+
+export default ProfileSettingContainer;
