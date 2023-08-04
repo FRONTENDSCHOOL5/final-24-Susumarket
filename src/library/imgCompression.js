b73cd4bbb4659a4da3f3690366012383a5f065e3
jongtae.nam <dark9737@gmail.com>
2023-08-04 14:46:04 +0900

    Feat: 이미지 압축을 위해 imgCompression 추가

---
+++ src/library/imgCompression.js
@@ -0,0 +1,23 @@
+import imageCompression from "browser-image-compression";
+
+export const uploadImgCompression = async (file) => {
+  const options = {
+    maxSizeMB: 10,
+    maxWidthOrHeight: 304,
+    useWebWorker:true,
+  }
+  const compressionImg = await imageCompression(file, options);
+  const preview = imageCompression.getDataUrlFromFile(compressionImg);
+  return {compressionImg, preview}
+}
+
+export const profileImgCompression = async (file) => {
+  const options = {
+    maxSizeMB: 10,
+    maxWidthOrHeight: 220,
+    useWebWorker:true,
+  }
+  const compressedFileBlob = await imageCompression(file, options);
+  const preview = await imageCompression.getDataUrlFromFile(compressedFileBlob);
+  return {compressedFileBlob, preview}
+}
