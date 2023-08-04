1a84b4cfe3ff6ddc771373c0eca25ebb91737e79
jongtae.nam <dark9737@gmail.com>
2023-08-04 14:40:29 +0900

    Feat: WebpContext 생성, detectWebpSupport 함수, resolveWebp 함수 생성, App 컴포넌트에 webpContext 및 detectWebSupport 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GlobalStyle } from "./GlobalStyle";
 import Router from "./routes/Router";
 import { UserContext } from "./context/UserContext.jsx";
 import { ModalContext } from "./context/ModalContext.jsx";
+import { WebpContext } from "./context/WebpContext";
+import { detectWebpSupport } from "./library/checkWebpSupport";
 function App() {
-  const [accessToken, setAccessToken] = useState(localStorage.getItem("accessToken")||null);
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken") || null,
+  );
   const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
   const [isOpenPostModal, setIsOpenPostModal] = useState(false);
+  const [isWebpSupported, setIsWebpSupported] = useState(false);
+
+  useEffect(() => {
+    detectWebpSupport();
+    if (document.body.classList.contains("webp")) {
+      setIsWebpSupported(true);
+    } else {
+      setIsWebpSupported(false);
+    }
+  }, []);
 
   return (
     <>
@@ -17,16 +31,18 @@ function App() {
           setAccessToken,
         }}
       >
-        <ModalContext.Provider
-          value={{
-            isOpenConfirmModal,
-            setIsOpenConfirmModal,
-            isOpenPostModal,
-            setIsOpenPostModal,
-          }}
-        >
-          <Router />
-        </ModalContext.Provider>
+        <WebpContext.Provider value={isWebpSupported}>
+          <ModalContext.Provider
+            value={{
+              isOpenConfirmModal,
+              setIsOpenConfirmModal,
+              isOpenPostModal,
+              setIsOpenPostModal,
+            }}
+          >
+            <Router />
+          </ModalContext.Provider>
+        </WebpContext.Provider>
       </UserContext.Provider>
     </>
   );
---
+++ src/context/WebpContext.jsx
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const WebpContext = createContext(null);
