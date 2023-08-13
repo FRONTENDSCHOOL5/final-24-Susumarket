b785f33462ddb3ac8f7b5807e728dd76adc98699
zkdk1241 <zkdk1241@naver.com>
2023-08-14 06:12:33 +0900

    Refactoring: productDetaio페이지 디자인패턴 적용

---
+++ src/pages/feed/product/productDetail/productDetail.container.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { productDetailAPI } from "../../../../API/productAPI";
+import ProductDetailPresenter from "./productDetail.presenter";
+import iconHeart from "../../../../img/icon-heart.svg";
+import iconHeartFill from "../../../../img/icon-heart-fill.svg";
+
+export default function ProductDetail() {
+  const [itemName, setItemName] = useState("");
+  const [price, setPrice] = useState("");
+  const [link, setLink] = useState("");
+  const [itemImage, setItemImage] = useState("");
+  const [name, setName] = useState("");
+  const [profile, setProfile] = useState("");
+  const [accountname, setAccountname] = useState("");
+  const [heartFill, setHeartFill] = useState(iconHeart);
+  const [isInValidPage, setIsInValidPage] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  const params = useParams();
+
+  const clickHeart = () => {
+    heartFill === iconHeart
+      ? setHeartFill(iconHeartFill)
+      : setHeartFill(iconHeart);
+  };
+
+  const navigate = useNavigate();
+
+  const newPrice = new Intl.NumberFormat().format(parseInt(price, 10));
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await productDetailAPI(params.productId);
+        setItemImage(data.itemImage);
+        setItemName(data.itemName);
+        setPrice(data.price);
+        setLink(data.link);
+        setName(data.author.username);
+        setProfile(data.author.image);
+        setAccountname(data.author.accountname);
+        setIsInValidPage(false);
+        setIsLoading(false);
+      } catch (error) {
+        setIsInValidPage(true);
+        console.error(error);
+      }
+    };
+
+    loadData();
+  }, [baseUrl, params]);
+
+  return (
+    <ProductDetailPresenter
+      itemName={itemName}
+      price={price}
+      link={link}
+      itemImage={itemImage}
+      name={name}
+      profile={profile}
+      accountname={accountname}
+      heartFill={heartFill}
+      isInValidPage={isInValidPage}
+      isLoading={isLoading}
+      clickHeart={clickHeart}
+      newPrice={newPrice}
+      navigate={navigate}
+    />
+  );
+}
---
+++ src/pages/feed/product/productDetail/productDetail.presenter.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import NewTopHeader from "../../../../components/commons/newTopHeader/NewTopHeader";
+import styled from "styled-components";
+import noImg from "../../../../img/no-image.png";
+import iconHeart from "../../../../img/icon-heart.svg";
+import iconHeartFill from "../../../../img/icon-heart-fill.svg";
+import Button from "../../../../components/commons/button/Button";
+import InvalidPage from "../../../../components/commons/inValidPage/InvaliPage";
+import Loading from "../../../../components/commons/loading/Loading";
+import {
+  ProfileDiv,
+  ContentDiv,
+  Btn,
+  Bottom,
+  Title,
+  Div,
+  Image,
+  Nickname,
+  Icon,
+  HeartIcon,
+  Profile,
+  Price,
+  Contents,
+  Account,
+} from "./productDetail.style.js";
+import profileImg from "../../../../img/ProfileImg.svg";
+
+export default function ProductDetailPresenter({
+  itemName,
+  price,
+  link,
+  itemImage,
+  name,
+  profile,
+  accountname,
+  heartFill,
+  isInValidPage,
+  isLoading,
+  clickHeart,
+  newPrice,
+  navigate,
+}) {
+  return (
+    <>
+      <NewTopHeader left={"back"} right={""} title={"상품 상세 페이지"} />
+      {isLoading ? (
+        <Loading />
+      ) : !isInValidPage ? (
+        <Div>
+          <Image
+            src={itemImage}
+            alt="상품 사진"
+            onError={(e) => {
+              e.target.src = noImg;
+            }}
+          />
+          <Profile>
+            <Icon
+              src={profile.includes("Ellipse.png") ? profileImg : profile}
+              alt="프로필 이미지"
+            />
+            <ContentDiv>
+              <ProfileDiv>
+                <Nickname>{name}</Nickname>
+                <Account>@{accountname} </Account>
+              </ProfileDiv>
+              <Title>{itemName}</Title>
+              <Contents>{link}</Contents>
+            </ContentDiv>
+          </Profile>
+          <Bottom>
+            <HeartIcon src={heartFill} onClick={clickHeart} alt="좋아요" />
+            <span
+              style={{
+                fontSize: "31px",
+                marginRight: "15px",
+                marginLeft: "15px",
+                color: "#CDCDCD",
+              }}
+            >
+              |
+            </span>
+            <Price>{newPrice}원</Price>
+            <Btn
+              className="ms"
+              onClick={() => {
+                navigate("/chatList/1");
+              }}
+            >
+              채팅하기
+            </Btn>
+          </Bottom>
+        </Div>
+      ) : (
+        <InvalidPage text={"현재 판매중인 상품이 아닙니다."} size={"large"} />
+      )}
+    </>
+  );
+}
