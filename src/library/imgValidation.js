388e32d6b3bf8ef739bacf88c56fb1666ed8d2c5
MAIN6419 <dark9737@gmail.com>
2023-08-13 20:41:25 +0900

    Feat: imgValidation 함수 customAlert 적용

--- src/library/imgValidation.js
+++ src/library/imgValidation.js
@@ -1,3 +1,5 @@
+import { sweetToast } from "./sweetAlert/sweetAlert";
+
 export const imgValidation = (file) => {
   const reg = /(.*?)\.(jpg|jpeg|png|gif|bmp|tif|heic)$/;
   // 파일 확인
@@ -6,19 +8,17 @@ export const imgValidation = (file) => {
   }
   // 파일 사이즈 확인
   if (file.size > 1024 * 1024 * 10) {
-    alert("이미지 파일의 크기를 초과하였습니다.(최대 10MB)");
+    sweetToast("이미지 파일의 크기를 초과하였습니다.(최대 10MB)", "warning");
     return false;
   }
   // 이미지 지원 형식 확인
-  if (
-    !reg.test(file.name)
-  ) {
-    alert(
-      "이미지 형식을 확인해 주세요!\n(지원형식 : .jpg,.gif, .png,.jpeg, .bmp,.tif, *.heic)"
+  if (!reg.test(file.name)) {
+    sweetToast(
+      "이미지 형식을 확인해 주세요!\n(지원형식 : .jpg,.gif, .png,.jpeg, .bmp,.tif, *.heic)",
+      "warning",
     );
     return false;
   }
   // 모두 만족 한다면 true 반환
   return true;
 };
-
